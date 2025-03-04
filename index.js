db7f8943ea4d971def3c041f686cfe611934b3e4
mehefujali <mehefujalim@gmail.com>
2025-03-04 13:42:35 +0530

    math master ai model created

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
-const express = require('express');
-const cors = require('cors')
-const app = express()
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-//model 
+//model
 
 const genAI = new GoogleGenerativeAI(process.env.AI_MODEL_API);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+  systemInstruction: " Your name math master your developer name is mehefuj ali his protfollio is mehefujali.com",
+});
 
 //mw
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // Routes
-app.get('/', (req, res) => {
-      res.send({status:true});
+
+app.get("/chat", async (req, res) => {
+  const prompt = req.query.prompt;
+  const result = await model.generateContent(prompt);
+  res.send(result.response.candidates);
+});
+app.get("/", (req, res) => {
+  res.send({ status: true });
 });
 
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
